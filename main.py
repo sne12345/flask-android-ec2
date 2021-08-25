303b68f80a2313e54852eb5827cdce41475fd44d
sne12345 <sne12345@naver.com>
2021-08-26 03:18:27 +0900

    /post api 수정

--- main.py
+++ main.py
@@ -9,41 +9,49 @@ import pyrebase
 app = Flask(__name__)
 
 # login으로 변경해야함
-# config = {
-#    "apiKey": "AIzaSyASyU5Qxe0zJZDVT1-oTYEVIcDYJzZNdA8",
-#    "authDomain": "aitutor-894f0.firebaseapp.com",
-#    "databaseURL": "https://aitutor-894f0-default-rtdb.firebaseio.com",
-#    "projectId": "aitutor-894f0",
-#    "storageBucket": "aitutor-894f0.appspot.com",
-#    "messagingSenderId": "145018138005",
-#    "appId": "1:145018138005:web:ea124ef3292ae2d0cb5a9c",
-#    "measurementId": "G-0YF0B8C5EC"
-# }
-#
-#
+config = {
+   "apiKey": "AIzaSyASyU5Qxe0zJZDVT1-oTYEVIcDYJzZNdA8",
+   "authDomain": "aitutor-894f0.firebaseapp.com",
+   "databaseURL": "https://aitutor-894f0-default-rtdb.firebaseio.com",
+   "projectId": "aitutor-894f0",
+   "storageBucket": "aitutor-894f0.appspot.com",
+   "messagingSenderId": "145018138005",
+   "appId": "1:145018138005:web:ea124ef3292ae2d0cb5a9c",
+   "measurementId": "G-0YF0B8C5EC"
+}
+
+
 # # db settings
-# firebase = pyrebase.initialize_app(config)
-# db = firebase.database()
-# db.child("member").child("124124124").child("test0").child("part1").push({
-#        "similarity":94,"pronunciation" : 41,
-#        "fluency" : 53,
-#        "expression" : 64,
-#        "relevance" : 72,
-#        "url" : "alkjlskd1111"})
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+db.child("member").child("124124124").child("test0").child("part1").push({
+       "similarity":94,"pronunciation" : 41,
+       "fluency" : 53,
+       "expression" : 64,
+       "relevance" : 72,
+       "url" : "alkjlskd1111"})
 
 
 @app.route('/')
 def hello_world():
     return render_template("main.html")
+    # return "Hello World"
 
 @app.route('/post',methods=["POST"])
 def hello_post():
-    value = request.form['android_id']
-    print(value)
-    value2 = request.form['url']
-    print(value2)
+    android_id = request.form['android_id']
+    print(android_id)
 
-    return ('서버 통신 :' + value+','+value2)
+    test_or_verify = request.form['test_or_verify']
+    print(test_or_verify)
+
+    part = request.form['part']
+    print(part)
+
+    url = request.form['url']
+    print(url)
+
+    return ('서버 통신 : ' + android_id+','+test_or_verify+','+part+','+url)
 
 @app.route('/one')
 def hello_one():
