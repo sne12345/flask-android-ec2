01104b3c1751bfc33f2300ac50ecef954fbcf74f
sne12345 <sne12345@naver.com>
2021-10-01 21:18:28 +0900

    edit main.py and score.py

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ def hello_post():
 
     # 파이어베이스 Storage에서 데이터 가져오기 => 파일 올라오는데는 시간 좀 걸림, 그래도 점수내기는 가능
     # 평가 후에 로컬에서 삭제하는 것도 구현할 것
-    path_on_storage_and_local = 'User' + android_id + '/' + url
-    storage.child(path_on_storage_and_local).download(path_on_storage_and_local)
+    # path_on_storage_and_local = 'User' + android_id + '/' + url
+    # storage.child(path_on_storage_and_local).download(path_on_storage_and_local)
 
     # 모의고사 점수내기
-    member_test_score = score.Member_Test()
-    total_score = member_test_score.evaluate(path_on_storage_and_local)
-    print(total_score)
+    # member_test_score = score.Member_Test()
+    # total_score = member_test_score.evaluate(path_on_storage_and_local)
+    # print(total_score)
 
 
     # 날짜, 시간 데이터 받아오기
@@ -59,14 +59,28 @@ def hello_post():
     android_db_id = "android_" + android_id
 
     # 안드스튜디오에서 다른 파트도 추가하면됨, 확인테스트도 봐야함
+    # db.child("member").child(android_db_id).child(test_type).update({
+    #     test_id : {
+    #         part_id : {
+    #             "similarity": total_score['유사도'],
+    #             "pronunciation": total_score['발음평가'],
+    #             "fluency": total_score['유창성'],
+    #             "expression": total_score['표현력'],
+    #             "relevance": total_score['주제의 연관성'],
+    #             "url": url[1:]
+    #         }
+    #     }
+    # })
+
+    # score.py 완성되기 전까지 서버 통신 코드
     db.child("member").child(android_db_id).child(test_type).update({
         test_id : {
             part_id : {
-                "similarity": total_score['유사도'],
-                "pronunciation": total_score['발음평가'],
-                "fluency": total_score['유창성'],
-                "expression": total_score['표현력'],
-                "relevance": total_score['주제의 연관성'],
+                "similarity": 100,
+                "pronunciation": 100,
+                "fluency": 100,
+                "expression": 100,
+                "relevance": 100,
                 "url": url[1:]
             }
         }
