358c95c1aedf1172ea305aa4af6651fa301cdb42
Ji Hyun <wus570708@naver.com>
2021-11-27 01:32:13 +0900

    발음평가 API용 테스트 코드

---
+++ api-test.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+import urllib3
+import json
+import base64
+
+#openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/Pronunciation"  # 영어
+openApiURL = "http://aiopen.etri.re.kr:8000/WiseASR/PronunciationKor" # 한국어
+
+accessKey = "ac679469-fbf1-4b08-abd7-f2aba1757ae6"
+audioFilePath = "Audio/001_034.pcm"
+languageCode = "korean"
+#script = "PRONUNCIATION_SCRIPT"
+
+file = open(audioFilePath, "rb")
+audioContents = base64.b64encode(file.read()).decode("utf8")
+file.close()
+
+requestJson = {
+    "access_key": accessKey,
+    "argument": {
+        "language_code": languageCode,
+        #"script": script,
+        "audio": audioContents
+    }
+}
+
+http = urllib3.PoolManager()
+response = http.request(
+    "POST",
+    openApiURL,
+    headers={"Content-Type": "application/json; charset=UTF-8"},
+    body=json.dumps(requestJson)
+)
+
+
+js = response.data
+y = json.loads(js)
+user = y["return_object"]['recognized']
+score = y["return_object"]['score']
+print(user,score)
