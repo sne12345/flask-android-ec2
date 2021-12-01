107ff3547cf7243d2f299c07b06ff6a656061989
Ubuntu <ubuntu@ip-172-31-52-19.ap-northeast-2.compute.internal>
2021-12-01 06:19:44 +0000

    score 서버 동작 확인 완료

--- request.py
+++ request.py
@@ -4,7 +4,8 @@ import time
 start = time.time()
 url = 'http://15.165.31.148:5000/score'
 answer = '올림픽 축구 대표팀이 내일 청소년 축구 대표팀과 평가전을'
-r = requests.get(url, json={'answer': answer})
+fname = '/home/ubuntu/flask-android-ec2/Audio/KOR_F_RM0769FLJH0325.mp3'
+r = requests.get(url, json={'fname':fname,'answer': answer})
 print(r)
 if r:
     r = r.json()
